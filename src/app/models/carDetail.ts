9b6cfbbba7656d099bd2c51c7c56dd753bb3f6cc
Abdulsamet ÖZDEMİR <samet-fener@hotmail.com>
2021-09-29 17:26:58 +0300

    21. Ders Ödevi/userImage ve setAdminRole eklenecek

--- src/app/models/carDetail.ts
+++ src/app/models/carDetail.ts
@@ -2,6 +2,8 @@ export interface CarDetail{
     carId:number;
     carName:string;
     brandName:string;
-    colorName:string;
+    engineDisplacement:string;
     dailyPrice:number;
+    modelYear:number;
+    minFindexPoint:number;
 }
